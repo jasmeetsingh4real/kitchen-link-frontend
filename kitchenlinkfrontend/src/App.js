5738ee0bb1e3c3ef137e94eb6368836dd2495a21
jasmeetsingh4real <jasmeetsingh4real1984@gmail.com>
2024-04-07 23:47:04 +0530

    created react app

---
+++ kitchenlinkfrontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import LoginPage from "./screens/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>JASMEET ❤️ KIRAT</h1>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
