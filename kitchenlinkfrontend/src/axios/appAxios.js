4f2620ea3de495eabb5395ca44c9b1ec8a74ac60
Jasmeet Singh <jasmeetsingh4real1984@gmail.com>
2024-04-20 01:23:25 +0530

    ..

--- kitchenlinkfrontend/src/axios/appAxios.js
+++ kitchenlinkfrontend/src/axios/appAxios.js
@@ -1,8 +1,16 @@
 import axios from "axios";
-
+import Cookies from "js-cookie";
 const Axios = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+Axios.interceptors.request.use((config) => {
+  const authToken = Cookies.get("authToken");
+  config.headers["authToken"] = authToken;
+  if (config.headers) {
+    return;
+  }
 });
